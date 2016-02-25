17c1f461218f25de713c16bf835173a5f28fc31a
Benoit Tellier <btellier@linagora.com>
2016-02-25 13:30:44 +0700

    IMAP-370 MOVE capability should be exposed and MoveProcessor enabled only if implemented by the MailboxManager

--- protocols/imap/src/main/java/org/apache/james/imap/processor/MoveProcessor.java
+++ protocols/imap/src/main/java/org/apache/james/imap/processor/MoveProcessor.java
@@ -1,6 +1,24 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
 package org.apache.james.imap.processor;
 
-import java.util.Collections;
 import java.util.List;
 
 import org.apache.james.imap.api.ImapConstants;
@@ -15,12 +33,15 @@ import org.apache.james.mailbox.exception.MailboxException;
 import org.apache.james.mailbox.model.MailboxPath;
 import org.apache.james.mailbox.model.MessageRange;
 
+import com.google.common.collect.ImmutableList;
+
 public class MoveProcessor extends AbstractMessageRangeProcessor<MoveRequest> implements CapabilityImplementingProcessor {
 
-	private static final List<String> CAPS = Collections.unmodifiableList(Collections.singletonList(ImapConstants.MOVE_COMMAND_NAME));
+	private final boolean moveCapabilitySupported;
 
 	public MoveProcessor(ImapProcessor next, MailboxManager mailboxManager, StatusResponseFactory factory) {
 		super(MoveRequest.class, next, mailboxManager, factory);
+        moveCapabilitySupported = mailboxManager.getSupportedCapabilities().contains(MailboxManager.Capabilities.Move);
 	}
 
     @Override
@@ -36,12 +57,13 @@ public class MoveProcessor extends AbstractMessageRangeProcessor<MoveRequest> im
         return "Move";
     }
 
-    /**
-    * @see org.apache.james.imap.processor.CapabilityImplementingProcessor
-    * #getImplementedCapabilities(org.apache.james.imap.api.process.ImapSession)
-    */
+    @Override
 	public List<String> getImplementedCapabilities(ImapSession session) {
-		return CAPS;
+        if (moveCapabilitySupported) {
+            return ImmutableList.of(ImapConstants.MOVE_COMMAND_NAME);
+        } else {
+            return ImmutableList.of();
+        }
 	}
 
 }
