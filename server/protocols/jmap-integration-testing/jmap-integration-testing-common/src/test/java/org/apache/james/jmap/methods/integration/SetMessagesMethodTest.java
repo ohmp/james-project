f5f21dfd97e13d950c2d69766b86e7c5f02b25fb
Benoit Tellier <btellier@linagora.com>
2016-06-03 16:28:56 +0700

    JAMES-1749 I can not delete messages without outbox

--- server/protocols/jmap-integration-testing/jmap-integration-testing-common/src/test/java/org/apache/james/jmap/methods/integration/SetMessagesMethodTest.java
+++ server/protocols/jmap-integration-testing/jmap-integration-testing-common/src/test/java/org/apache/james/jmap/methods/integration/SetMessagesMethodTest.java
@@ -108,6 +108,33 @@ public abstract class SetMessagesMethodTest {
     }
 
     @Test
+    public void iCanNotDeleteMessagesWithoutOutBox() throws Exception {
+        // Given
+        String user2 = "user2@" + USERS_DOMAIN;
+        String password = "password";
+        jmapServer.serverProbe().addUser(user2, password);
+        AccessToken user2AccessToken = JmapAuthentication.authenticateJamesUser(user2, password);
+
+        jmapServer.serverProbe().createMailbox(MailboxConstants.USER_NAMESPACE, user2, "mailbox");
+
+        jmapServer.serverProbe().appendMessage(user2, new MailboxPath(MailboxConstants.USER_NAMESPACE, user2, "mailbox"),
+            new ByteArrayInputStream("Subject: test\r\n\r\ntestmail".getBytes()), new Date(), false, new Flags());
+        await();
+
+        //Then
+        String presumedMessageId = user2 + "|mailbox|1";
+        given()
+            .accept(ContentType.JSON)
+            .contentType(ContentType.JSON)
+            .header("Authorization", user2AccessToken.serialize())
+            .body("[[\"setMessages\", {\"destroy\": [\"" + presumedMessageId + "\"]}, \"#0\"]]")
+        .when()
+            .post("/jmap")
+        .then()
+            .statusCode(500);
+    }
+
+    @Test
     public void iCanNotMarkMessagesAsReadWithoutOutBox() throws Exception {
         // Given
         String user2 = "user2@" + USERS_DOMAIN;
