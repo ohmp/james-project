c121d905c01c6635431f411e435d5cf521b1acd8
Benoit Tellier <btellier@linagora.com>
2016-11-17 15:23:37 +0700

    DUMMY (to be removed) make cassandra project compile after messageIdMapper changes

--- mailbox/cassandra/src/main/java/org/apache/james/mailbox/cassandra/mail/CassandraMessageIdMapper.java
+++ mailbox/cassandra/src/main/java/org/apache/james/mailbox/cassandra/mail/CassandraMessageIdMapper.java
@@ -18,34 +18,25 @@
  ****************************************************************/
 package org.apache.james.mailbox.cassandra.mail;
 
-import java.util.Comparator;
 import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-import java.util.function.Function;
-import java.util.stream.Stream;
+import java.util.Map;
 
-import org.apache.commons.lang3.tuple.Pair;
-import org.apache.james.mailbox.cassandra.CassandraId;
-import org.apache.james.mailbox.cassandra.CassandraMessageId;
+import javax.mail.Flags;
+
+import org.apache.commons.lang.NotImplementedException;
+import org.apache.james.mailbox.MessageManager;
 import org.apache.james.mailbox.exception.MailboxException;
 import org.apache.james.mailbox.exception.MailboxNotFoundException;
-import org.apache.james.mailbox.model.ComposedMessageId;
-import org.apache.james.mailbox.model.ComposedMessageIdWithMetaData;
 import org.apache.james.mailbox.model.MailboxId;
-import org.apache.james.mailbox.model.MessageAttachment;
 import org.apache.james.mailbox.model.MessageId;
+import org.apache.james.mailbox.model.MessageMetaData;
+import org.apache.james.mailbox.model.UpdatedFlags;
 import org.apache.james.mailbox.store.mail.AttachmentMapper;
 import org.apache.james.mailbox.store.mail.MailboxMapper;
 import org.apache.james.mailbox.store.mail.MessageIdMapper;
 import org.apache.james.mailbox.store.mail.MessageMapper.FetchType;
+import org.apache.james.mailbox.store.mail.model.Mailbox;
 import org.apache.james.mailbox.store.mail.model.MailboxMessage;
-import org.apache.james.mailbox.store.mail.model.Message;
-import org.apache.james.mailbox.store.mail.model.impl.SimpleMailboxMessage;
-
-import com.github.fge.lambdas.Throwing;
-import com.github.fge.lambdas.functions.FunctionChainer;
-import com.github.steveash.guavate.Guavate;
 
 public class CassandraMessageIdMapper implements MessageIdMapper {
 
@@ -65,65 +56,32 @@ public class CassandraMessageIdMapper implements MessageIdMapper {
     }
 
     @Override
-    public List<Message> find(List<MessageId> messageIds, FetchType fetchType) {
-        List<ComposedMessageIdWithMetaData> composedMessageIds = messageIds.stream()
-            .map(messageId -> imapUidDAO.retrieve((CassandraMessageId) messageId, Optional.empty()))
-            .flatMap(CompletableFuture::join)
-            .collect(Guavate.toImmutableList());
-        return messageDAO.retrieveMessages(composedMessageIds, fetchType, Optional.empty()).join()
-                .map(loadAttachments())
-                .map(toMailboxMessages())
-                .sorted(Comparator.comparing(MailboxMessage::getUid))
-                .collect(Guavate.toImmutableList());
-    }
-
-    private Function<Pair<MailboxMessage, Stream<MessageAttachmentById>>, Pair<MailboxMessage, Stream<MessageAttachment>>> loadAttachments() {
-        return pair -> Pair.of(pair.getLeft(), new AttachmentLoader(attachmentMapper).getAttachments(pair.getRight().collect(Guavate.toImmutableList())));
-    }
-
-    private FunctionChainer<Pair<MailboxMessage, Stream<MessageAttachment>>, SimpleMailboxMessage> toMailboxMessages() {
-        return Throwing.function(pair -> {
-            return SimpleMailboxMessage.cloneWithAttachments(pair.getLeft(), 
-                    pair.getRight().collect(Guavate.toImmutableList()));
-        });
+    public List<MailboxMessage> find(List<MessageId> messageIds, FetchType fetchType) {
+        throw new NotImplementedException();
     }
 
     @Override
     public List<MailboxId> findMailboxes(MessageId messageId) {
-        return imapUidDAO.retrieve((CassandraMessageId) messageId, Optional.empty()).join()
-            .map(ComposedMessageIdWithMetaData::getComposedMessageId)
-            .map(ComposedMessageId::getMailboxId)
-            .collect(Guavate.toImmutableList());
+        throw new NotImplementedException();
     }
 
     @Override
     public void save(MailboxMessage mailboxMessage) throws MailboxNotFoundException, MailboxException {
-        CassandraId mailboxId = (CassandraId) mailboxMessage.getMailboxId();
-        messageDAO.save(mailboxMapper.findMailboxById(mailboxId), mailboxMessage).join();
-        CassandraMessageId messageId = (CassandraMessageId) mailboxMessage.getMessageId();
-        ComposedMessageIdWithMetaData composedMessageIdWithMetaData = ComposedMessageIdWithMetaData.builder()
-                    .composedMessageId(new ComposedMessageId(mailboxId, messageId, mailboxMessage.getUid()))
-                    .flags(mailboxMessage.createFlags())
-                    .modSeq(mailboxMessage.getModSeq())
-                    .build();
-        CompletableFuture.allOf(imapUidDAO.insert(composedMessageIdWithMetaData),
-                messageIdDAO.insert(composedMessageIdWithMetaData))
-        .join();
+        throw new NotImplementedException();
     }
 
     @Override
     public void delete(MessageId messageId) {
-        CassandraMessageId cassandraMessageId = (CassandraMessageId) messageId;
-        messageDAO.delete(cassandraMessageId).join();
-        imapUidDAO.retrieve(cassandraMessageId, Optional.empty()).join()
-            .map(ComposedMessageIdWithMetaData::getComposedMessageId)
-            .forEach(composedMessageId -> deleteIds(composedMessageId).join());
+        throw new NotImplementedException();
     }
 
-    private CompletableFuture<Void> deleteIds(ComposedMessageId composedMessageId) {
-        CassandraMessageId messageId = (CassandraMessageId) composedMessageId.getMessageId();
-        CassandraId mailboxId = (CassandraId) composedMessageId.getMailboxId();
-        return CompletableFuture.allOf(imapUidDAO.delete(messageId, mailboxId),
-                messageIdDAO.delete(mailboxId, composedMessageId.getUid()));
+    @Override
+    public void delete(MessageId messageId, List<MailboxId> mailboxIds) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public Map<MailboxId, UpdatedFlags> setFlags(Flags newState, MessageManager.FlagsUpdateMode updateMode, MessageId messageId) throws MailboxException {
+        throw new NotImplementedException();
     }
 }
