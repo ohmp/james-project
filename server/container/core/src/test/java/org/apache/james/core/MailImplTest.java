724dc7096348b9d08a9457bf97290e6bb5cd7ee3
benwa <btellier@linagora.com>
2017-05-31 10:24:12 +0700

    JAMES-2040 Enhance tests for MailImpl

--- server/container/core/src/test/java/org/apache/james/core/MailImplTest.java
+++ server/container/core/src/test/java/org/apache/james/core/MailImplTest.java
@@ -18,27 +18,23 @@
  ****************************************************************/
 package org.apache.james.core;
 
-import org.apache.mailet.base.test.MailUtil;
-import org.apache.mailet.MailAddress;
-import org.apache.mailet.Mail;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
 
 import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.internet.MimeMessage;
 
-import java.io.ByteArrayInputStream;
-import java.util.ArrayList;
-import static org.junit.Assert.*;
+import org.apache.mailet.Mail;
+import org.apache.mailet.MailAddress;
+import org.apache.mailet.base.test.MailUtil;
 import org.junit.Test;
 
-public class MailImplTest extends MailTestAllImplementations {
+public class MailImplTest {
 
     private static final Session NO_SESSION = null;
-    
-    @Override
-    protected Mail createMailImplementation() {
-        return new MailImpl();
-    }
 
     @Test
     public void testConstr1() throws MessagingException {
@@ -46,9 +42,10 @@ public class MailImplTest extends MailTestAllImplementations {
 
         helperTestInitialState(mail);
         helperTestMessageSize(mail, 0); // MimeMessageWrapper default is 0
-        assertNull("no initial message", mail.getMessage());
-        assertNull("no initial sender", mail.getSender());
-        assertNull("no initial name", mail.getName());
+
+        assertThat(mail.getMessage()).as("Checking there is an initial message").isNull();
+        assertThat(mail.getSender()).as("Checking there is an initial sender").isNull();
+        assertThat(mail.getName()).as("Checking there is an initial name").isNull();
     }
 
     @Test
@@ -61,12 +58,22 @@ public class MailImplTest extends MailTestAllImplementations {
 
         helperTestInitialState(mail); // MimeMessageWrapper default is 0
         helperTestMessageSize(mail, 0); // MimeMessageWrapper default is 0
-        assertNull("no initial message", mail.getMessage());
-        assertEquals("sender", sender, mail.getSender().toString());
-        assertEquals("name", name, mail.getName());
+        assertThat(mail.getMessage()).as("No initial message when not specified").isNull();
+        assertThat(mail.getSender().asString()).as("Sender should match original value").isEqualTo(sender);
+        assertThat(mail.getName()).as("Name should be the specified one").isEqualTo(name);
+    }
+
+    @Test
+    public void setMessageShouldaddAMessage() throws MessagingException {
+        ArrayList<MailAddress> recepients = new ArrayList<MailAddress>();
+        String name = MailUtil.newId();
+        String sender = "sender@localhost";
+        MailAddress senderMailAddress = new MailAddress(sender);
+        MailImpl mail = new MailImpl(name, senderMailAddress, recepients);
 
         mail.setMessage(new MimeMessage(NO_SESSION));
-        assertNotNull("message", mail.getMessage());
+
+        assertThat(mail.getMessage()).as("Message should not be null when specified").isNotNull();
     }
 
     @Test
@@ -80,31 +87,173 @@ public class MailImplTest extends MailTestAllImplementations {
 
         helperTestInitialState(mail);
         helperTestMessageSize(mail, 0);
-        assertEquals("initial message", mimeMessage.getMessageID(), mail.getMessage().getMessageID());
-        assertEquals("sender", sender, mail.getSender().toString());
-        assertEquals("name", name, mail.getName());
+        assertThat(mail.getMessage().getMessageID()).isEqualTo(mimeMessage.getMessageID());
+        assertThat(mail.getSender().asString()).isEqualTo(sender);
+        assertThat(mail.getName()).isEqualTo(name);
         mail.dispose();
     }
 
     @Test
-    public void testDuplicate() throws MessagingException {
+    public void duplicateShouldCreateANewJavaObject() throws MessagingException {
         MailImpl mail = new MailImpl();
         MailImpl duplicate = (MailImpl) mail.duplicate();
-        assertNotSame("is real duplicate", mail, duplicate);
+
+        assertThat(mail).as("duplicate method should return different objects").isNotSameAs(duplicate);
+
         helperTestInitialState(duplicate);
         helperTestMessageSize(duplicate, 0);
     }
 
     @Test
-    public void testDuplicateNewName() throws MessagingException {
+    public void duplicateShouldNotChangeOriginalMailName() throws MessagingException {
         String newName = "aNewName";
-
         MailImpl mail = new MailImpl();
-        assertFalse("before + after names differ", newName.equals(mail.getName()));
+        assertThat(mail.getName()).as("before + after names should differ").isNotEqualTo(newName);
 
-        MailImpl duplicate = (MailImpl) mail.duplicate(newName);
-        assertEquals("new name set", newName, duplicate.getName());
-        helperTestInitialState(duplicate);
-        helperTestMessageSize(duplicate, 0);
+        mail.duplicate(newName);
+
+        assertThat(mail.getName()).as("new name should be set").isNotEqualTo(newName);
+    }
+
+    @Test
+    public void hasAttributesShouldBeFalseByDefault() {
+        Mail mail = new MailImpl();
+
+        assertThat(mail.hasAttributes()).isFalse();
+    }
+
+    @Test
+    public void getAttributeNamesShouldBeEmptyByDefault() {
+        Mail mail = new MailImpl();
+
+        assertThat(mail.getAttributeNames()).isEmpty();
+    }
+
+    @Test
+    public void getAttributeShouldReturnNullWhenAttributeAbsent() {
+        Mail mail = new MailImpl();
+
+        assertThat(mail.getAttribute("test")).isNull();
+    }
+
+    @Test
+    public void setAttributeShouldReturnNullWhenNoPreviousValue() {
+        Mail mail = new MailImpl();
+
+        assertThat(mail.setAttribute("test", "value")).isNull();
+    }
+
+    @Test
+    public void getAttributeShouldReturnStoredValue() {
+        Mail mail = new MailImpl();
+
+        String value = "value";
+        String key = "test";
+        mail.setAttribute(key, value);
+
+        assertThat(mail.getAttribute(key)).isEqualTo(value);
+    }
+
+    @Test
+    public void setAttributeShouldReturnPreviouslyStoredValue() {
+        Mail mail = new MailImpl();
+
+        String value = "value";
+        String key = "test";
+        mail.setAttribute(key, value);
+
+        assertThat(mail.setAttribute(key, "newValue")).isEqualTo(value);
+    }
+
+    @Test
+    public void removeAttributeShouldReturnNullWhenAbsent() {
+        Mail mail = new MailImpl();
+
+        assertThat(mail.removeAttribute("test")).isNull();
+    }
+
+    @Test
+    public void removeAttributeShouldReturnPreviouslyStoredValue() {
+        Mail mail = new MailImpl();
+
+        String value = "value";
+        String key = "test";
+        mail.setAttribute(key, value);
+
+        assertThat(mail.removeAttribute(key)).isEqualTo(value);
+    }
+
+    @Test
+    public void hasAttributesShouldReturnTrueWhenHasAttribute() {
+        Mail mail = new MailImpl();
+
+        String value = "value";
+        String key = "test";
+        mail.setAttribute(key, value);
+
+        assertThat(mail.hasAttributes()).isTrue();
+    }
+
+    @Test
+    public void getAttributeNamesShouldReturnStoredAttributesName() {
+        Mail mail = new MailImpl();
+
+        String value = "value";
+        String key = "test";
+        mail.setAttribute(key, value);
+
+        assertThat(mail.getAttributeNames()).containsExactly(key);
+    }
+
+    @Test
+    public void removeAttributeShouldRemoveSpecificAttribute() {
+        Mail mail = new MailImpl();
+
+        String value = "value";
+        String key1 = "test1";
+        String key2 = "test2";
+        mail.setAttribute(key1, value);
+        mail.setAttribute(key2, value);
+        mail.removeAttribute(key1);
+
+        assertThat(mail.getAttributeNames()).containsExactly(key2);
+    }
+
+    @Test
+    public void removeAllAttributesShouldClearMailAttributes() {
+        Mail mail = new MailImpl();
+
+        String value = "value";
+        String key1 = "test1";
+        String key2 = "test2";
+        mail.setAttribute(key1, value);
+        mail.setAttribute(key2, value);
+        mail.removeAllAttributes();
+
+        assertThat(mail.getAttributeNames()).isEmpty();
+    }
+
+    protected void helperTestInitialState(Mail mail) {
+        assertThat(mail.hasAttributes()).as("Expecting no initial attributes").isFalse();
+        assertThat(mail.getErrorMessage()).as("Expecting no initial error").isNull();
+        assertThat(mail.getLastUpdated()).as("Expecting initial last update set").isNotNull();
+        try {
+            mail.getRecipients().isEmpty();
+        } catch (NullPointerException e) {
+            // current behavior. *BUT*, shouldn't this method better return with
+            // an empty list?!
+        }
+        assertThat(mail.getRemoteAddr()).as("initial remote address should be localhost ip").isEqualTo("127.0.0.1");
+        assertThat(mail.getRemoteHost()).as("initial remote host should be localhost").isEqualTo("localhost");
+        assertThat(mail.getState()).as("Expecting default initial state").isEqualTo(Mail.DEFAULT);
+    }
+
+    protected void helperTestMessageSize(Mail mail, int expectedMsgSize) throws MessagingException {
+        try {
+            assertThat(mail.getMessageSize()).isEqualTo(expectedMsgSize);
+        } catch (NullPointerException e) {
+            // current behavior. *BUT*, shouldn't this method return more
+            // gracefully?!
+        }
     }
 }
