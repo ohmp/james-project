be010fb861348b9c6a3ce76259157643526252e9
benwa <btellier@linagora.com>
2017-06-22 10:36:30 +0700

    POC Cassandra streaming

---
+++ backends-common/cassandra/src/main/java/org/apache/james/backends/cassandra/blobs/CassandraBlobMapper.java
@@ -0,0 +1,138 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.backends.cassandra.blobs;
+
+import static com.datastax.driver.core.querybuilder.QueryBuilder.eq;
+import static com.datastax.driver.core.querybuilder.QueryBuilder.insertInto;
+import static com.datastax.driver.core.querybuilder.QueryBuilder.select;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.SequenceInputStream;
+import java.nio.ByteBuffer;
+import java.util.Enumeration;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.james.backends.cassandra.utils.CassandraAsyncExecutor;
+import org.apache.james.util.FluentFutureStream;
+
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.utils.UUIDs;
+import com.google.common.base.Preconditions;
+
+public class CassandraBlobMapper {
+
+
+    public static final int CHUNK_SIZE = 1024;
+    private final CassandraAsyncExecutor executor;
+
+    public CassandraBlobMapper(Session session) {
+        this.executor = new CassandraAsyncExecutor(session);
+    }
+
+    public CompletableFuture<UUID> write(byte[] data) {
+        UUID uuid = UUIDs.timeBased();
+        return FluentFutureStream.of(
+            computeBlobParts(data)
+                .map(pair ->
+                    executor.executeVoid(insertInto(CassandraBlobModule.TABLE_NAME)
+                        .value(CassandraBlobModule.ID, uuid)
+                        .value(CassandraBlobModule.PART, pair.getKey())
+                        .value(CassandraBlobModule.DATA, pair.getValue()))))
+            .completableFuture()
+        .thenApply(any -> uuid);
+    }
+
+    private Stream<Pair<Integer, ByteBuffer>> computeBlobParts(byte[] data) {
+        int size = data.length;
+        int fullChunkCount = size / CHUNK_SIZE;
+
+        return Stream.concat(
+            IntStream.range(0, fullChunkCount)
+                .mapToObj(i -> Pair.of(i, ByteBuffer.wrap(data, i * CHUNK_SIZE, CHUNK_SIZE))),
+            computeFinalByteBuffer(data, fullChunkCount * CHUNK_SIZE, fullChunkCount));
+    }
+
+    private Stream<Pair<Integer, ByteBuffer>> computeFinalByteBuffer(byte[] data, int offset, int index) {
+        if (offset == data.length) {
+            return Stream.of();
+        }
+        return Stream.of(Pair.of(index, ByteBuffer.wrap(data, offset, data.length - offset)));
+    }
+
+    public InputStream read(UUID id) {
+        return new SequenceInputStream(
+            new CassandraBackedInputStreamEnumeration(
+                i -> readPart(id, i)));
+    }
+
+    public CompletableFuture<Optional<Row>> readPart(UUID uuid, int part) {
+        return executor.executeSingleRow(select().from(CassandraBlobModule.TABLE_NAME)
+            .where(eq(CassandraBlobModule.ID, uuid))
+            .and(eq(CassandraBlobModule.PART, part)));
+    }
+
+    public static class CassandraBackedInputStreamEnumeration implements Enumeration<InputStream> {
+
+        private final Function<Integer, CompletableFuture<Optional<Row>>> partFetcher;
+        private int nextPartOffset;
+        private CompletableFuture<Optional<Row>> preFetch;
+        private Optional<Row> nextRow;
+
+        public CassandraBackedInputStreamEnumeration(Function<Integer, CompletableFuture<Optional<Row>>> partFetcher) {
+            this.partFetcher = partFetcher;
+            this.nextPartOffset = 0;
+            this.nextRow = Optional.empty();
+            fetch();
+        }
+
+        private void fetch() {
+            preFetch = partFetcher.apply(nextPartOffset);
+            nextPartOffset++;
+        }
+
+        @Override
+        public boolean hasMoreElements() {
+            CompletableFuture<Optional<Row>> nextRowFuture = preFetch;
+            fetch();
+            nextRow = nextRowFuture.join();
+            return nextRow.isPresent();
+        }
+
+        @Override
+        public InputStream nextElement() {
+            Preconditions.checkState(nextRow.isPresent(), "Expecting content while calling nextElement. Have you called 'hasMoreElements' before?");
+            return new ByteArrayInputStream(getRowContent(nextRow.get()));
+        }
+
+        private byte[] getRowContent(Row row) {
+            byte[] data = new byte[row.getBytes(CassandraBlobModule.DATA).remaining()];
+            row.getBytes(CassandraBlobModule.DATA).get(data);
+            return data;
+        }
+    }
+}
---
+++ backends-common/cassandra/src/test/java/org/apache/james/backends/cassandra/blobs/CassandraBlobMapperTest.java
@@ -0,0 +1,92 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.backends.cassandra.blobs;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.james.backends.cassandra.CassandraCluster;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.datastax.driver.core.utils.UUIDs;
+import com.google.common.base.Charsets;
+import com.google.common.base.Strings;
+
+public class CassandraBlobMapperTest {
+
+    private CassandraCluster cassandra;
+    private CassandraBlobMapper testee;
+
+    @Before
+    public void setUp() {
+        cassandra = CassandraCluster.create(new CassandraBlobModule());
+        cassandra.ensureAllTables();
+
+        testee = new CassandraBlobMapper(cassandra.getConf());
+    }
+
+    @After
+    public void tearDown() {
+        cassandra.clearAllTables();
+        cassandra.close();
+    }
+
+    @Test
+    public void test() throws IOException {
+        byte[] bytes = IOUtils.toByteArray(testee.read(UUIDs.timeBased()));
+
+        assertThat(bytes).isEmpty();
+    }
+
+    @Test
+    public void test1() throws IOException {
+        UUID uuid = testee.write("toto".getBytes(Charsets.UTF_8)).join();
+
+        byte[] bytes = IOUtils.toByteArray(testee.read(uuid));
+
+        assertThat(new String(bytes, Charsets.UTF_8)).isEqualTo("toto");
+    }
+
+    @Test
+    public void test2() throws IOException {
+        String longString = Strings.repeat("0123456789\n", 1000);
+        UUID uuid = testee.write(longString.getBytes(Charsets.UTF_8)).join();
+
+        byte[] bytes = IOUtils.toByteArray(testee.read(uuid));
+
+        assertThat(new String(bytes, Charsets.UTF_8)).isEqualTo(longString);
+    }
+
+    @Test
+    public void test3() throws IOException {
+        String longString = Strings.repeat("0123456789\n", CassandraBlobMapper.CHUNK_SIZE);
+        UUID uuid = testee.write(longString.getBytes(Charsets.UTF_8)).join();
+
+        byte[] bytes = IOUtils.toByteArray(testee.read(uuid));
+
+        assertThat(new String(bytes, Charsets.UTF_8)).isEqualTo(longString);
+    }
+
+}
