4f0bfc3704a25b4ff7c18fab0b1e9d43ebcf80f5
Antoine Duprat <aduprat@linagora.com>
2017-10-02 15:09:01 +0200

    #327 Introduce Right search capability

--- mailbox/api/src/main/java/org/apache/james/mailbox/MailboxManager.java
+++ mailbox/api/src/main/java/org/apache/james/mailbox/MailboxManager.java
@@ -80,7 +80,8 @@ public interface MailboxManager extends RequestAware, MailboxListenerSupport {
         Annotation,
         Move,
         Namespace,
-        UserFlag
+        UserFlag,
+        RightSearch
     }
 
     EnumSet<MailboxCapabilities> getSupportedMailboxCapabilities();
