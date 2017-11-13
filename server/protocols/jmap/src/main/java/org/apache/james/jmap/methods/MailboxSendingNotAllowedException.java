0fe56e0954ef1a6f7e74fbb8ec02c2a91b0789f9
benwa <btellier@linagora.com>
2017-11-13 16:23:17 +0700

    JAMES-2214 Only one From address is supported: the user name

--- server/protocols/jmap/src/main/java/org/apache/james/jmap/methods/MailboxSendingNotAllowedException.java
+++ server/protocols/jmap/src/main/java/org/apache/james/jmap/methods/MailboxSendingNotAllowedException.java
@@ -19,20 +19,18 @@
 
 package org.apache.james.jmap.methods;
 
-import java.util.Collection;
-
 import org.apache.james.mailbox.exception.MailboxException;
 
 public class MailboxSendingNotAllowedException extends MailboxException {
 
-    private Collection<String> allowedFroms;
+    private String allowedFrom;
 
-    public MailboxSendingNotAllowedException(Collection<String> allowedFroms) {
+    public MailboxSendingNotAllowedException(String allowedFroms) {
         super();
-        this.allowedFroms = allowedFroms;
+        this.allowedFrom = allowedFroms;
     }
     
-    public Collection<String> getAllowedFroms() {
-        return allowedFroms;
+    public String getAllowedFrom() {
+        return allowedFrom;
     }
 }
