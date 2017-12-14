7a4670172be876623fe4766c4a971ed0e21b558d
benwa <btellier@linagora.com>
2017-12-14 14:45:21 +0700

    JAMES-2266 Implement a task for merging mailboxes
    We merges mailboxes content and rights

---
+++ mailbox/cassandra/src/main/java/org/apache/james/mailbox/cassandra/mail/task/MailboxMergingTask.java
@@ -0,0 +1,73 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mailbox.cassandra.mail.task;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+import org.apache.james.mailbox.cassandra.ids.CassandraId;
+import org.apache.james.mailbox.exception.MailboxException;
+import org.apache.james.util.MDCBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Throwables;
+
+public class MailboxMergingTask implements Task {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MailboxMergingTask.class);
+    public static final String MAILBOX_MERGING = "mailboxMerging";
+    public static final String OLD_MAILBOX_ID = "oldMailboxId";
+    public static final String NEW_MAILBOX_ID = "newMailboxId";
+
+    private final MailboxMergingTaskRunner taskRunner;
+    private final CassandraId oldMailboxId;
+    private final CassandraId newMailboxId;
+
+    public MailboxMergingTask(MailboxMergingTaskRunner taskRunner, CassandraId oldMailboxId, CassandraId newMailboxId) {
+        this.taskRunner = taskRunner;
+        this.oldMailboxId = oldMailboxId;
+        this.newMailboxId = newMailboxId;
+    }
+
+    @Override
+    public Result run() {
+        TaskId taskId = Task.generateTaskId();
+        try (Closeable mdc = MDCBuilder.create()
+                 .addContext(TASK_ID, taskId)
+                 .addContext(TASK_TYPE, MAILBOX_MERGING)
+                 .addContext(OLD_MAILBOX_ID, oldMailboxId)
+                 .addContext(NEW_MAILBOX_ID, newMailboxId)
+                .build()) {
+            return doRun();
+        } catch (IOException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+
+    private Result doRun() {
+        try {
+            return taskRunner.run(oldMailboxId, newMailboxId)
+                .ifCompleted(() -> LOGGER.info("Mailbox merging succeeded"));
+        } catch (MailboxException e) {
+            LOGGER.warn("Mailbox merging failed", e);
+            return Result.PARTIAL;
+        }
+    }
+}
---
+++ mailbox/cassandra/src/main/java/org/apache/james/mailbox/cassandra/mail/task/MailboxMergingTaskRunner.java
@@ -0,0 +1,94 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mailbox.cassandra.mail.task;
+
+import javax.inject.Inject;
+
+import org.apache.james.mailbox.MailboxManager;
+import org.apache.james.mailbox.MailboxSession;
+import org.apache.james.mailbox.acl.ACLDiff;
+import org.apache.james.mailbox.cassandra.ids.CassandraId;
+import org.apache.james.mailbox.cassandra.mail.CassandraACLMapper;
+import org.apache.james.mailbox.cassandra.mail.CassandraMailboxDAO;
+import org.apache.james.mailbox.cassandra.mail.CassandraMessageIdDAO;
+import org.apache.james.mailbox.cassandra.mail.CassandraUserMailboxRightsDAO;
+import org.apache.james.mailbox.exception.MailboxException;
+import org.apache.james.mailbox.model.ComposedMessageId;
+import org.apache.james.mailbox.model.ComposedMessageIdWithMetaData;
+import org.apache.james.mailbox.model.MailboxACL;
+import org.apache.james.mailbox.model.MessageRange;
+import org.apache.james.mailbox.store.StoreMessageIdManagerNoChecks;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MailboxMergingTaskRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MailboxMergingTaskRunner.class);
+
+    private final MailboxManager mailboxManager;
+    private final StoreMessageIdManagerNoChecks messageIdManager;
+    private final CassandraMessageIdDAO cassandraMessageIdDAO;
+    private final CassandraMailboxDAO mailboxDAO;
+    private final CassandraUserMailboxRightsDAO rightsDAO;
+    private final CassandraACLMapper cassandraACLMapper;
+
+    @Inject
+    public MailboxMergingTaskRunner(MailboxManager mailboxManager, StoreMessageIdManagerNoChecks messageIdManager, CassandraMessageIdDAO cassandraMessageIdDAO, CassandraMailboxDAO mailboxDAO, CassandraUserMailboxRightsDAO rightsDAO, CassandraACLMapper cassandraACLMapper) {
+        this.mailboxManager = mailboxManager;
+        this.messageIdManager = messageIdManager;
+        this.cassandraMessageIdDAO = cassandraMessageIdDAO;
+        this.mailboxDAO = mailboxDAO;
+        this.rightsDAO = rightsDAO;
+        this.cassandraACLMapper = cassandraACLMapper;
+    }
+
+    public Task.Result run(CassandraId oldMailboxId, CassandraId newMailboxId) throws MailboxException {
+        MailboxSession session = mailboxManager.createSystemSession("task");
+        return moveMessages(oldMailboxId, newMailboxId, session)
+            .ifCompleted(() -> mailboxDAO.delete(oldMailboxId).join())
+            .ifCompleted(() -> mergeRights(oldMailboxId, newMailboxId));
+    }
+
+    private Task.Result moveMessages(CassandraId oldMailboxId, CassandraId newMailboxId, MailboxSession session) {
+        return cassandraMessageIdDAO.retrieveMessages(oldMailboxId, MessageRange.all())
+            .join()
+            .map(ComposedMessageIdWithMetaData::getComposedMessageId)
+            .map(messageId -> moveMessage(newMailboxId, messageId, session))
+            .reduce(Task.Result.COMPLETED, Task::combine);
+    }
+
+    private Task.Result moveMessage(CassandraId newMailboxId, ComposedMessageId composedMessageId, MailboxSession session) {
+        try {
+            messageIdManager.setInMailboxesNoCheck(composedMessageId.getMessageId(), newMailboxId, session);
+            return Task.Result.COMPLETED;
+        } catch (MailboxException e) {
+            LOGGER.warn("Failed moving message {}", composedMessageId.getMessageId(), e);
+            return Task.Result.PARTIAL;
+        }
+    }
+
+    private void mergeRights(CassandraId oldMailboxId, CassandraId newMailboxId) throws MailboxException {
+        MailboxACL oldAcl = cassandraACLMapper.getACL(oldMailboxId).join();
+        MailboxACL newAcl = cassandraACLMapper.getACL(newMailboxId).join();
+        MailboxACL finalAcl = newAcl.union(oldAcl);
+
+        cassandraACLMapper.setACL(newMailboxId, finalAcl);
+        rightsDAO.update(oldMailboxId, ACLDiff.computeDiff(oldAcl, MailboxACL.EMPTY)).join();
+    }
+}
