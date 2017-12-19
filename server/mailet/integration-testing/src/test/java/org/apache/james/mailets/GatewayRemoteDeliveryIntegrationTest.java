36f52aa9c1dce8ecd377c3e3e8942dd69a531a1b
benwa <btellier@linagora.com>
2017-12-19 14:04:33 +0700

    Remove enableJmx where not needed

--- server/mailet/integration-testing/src/test/java/org/apache/james/mailets/GatewayRemoteDeliveryIntegrationTest.java
+++ server/mailet/integration-testing/src/test/java/org/apache/james/mailets/GatewayRemoteDeliveryIntegrationTest.java
@@ -278,7 +278,6 @@ public class GatewayRemoteDeliveryIntegrationTest {
                 .addProcessor(root())
                 .addProcessor(CommonProcessors.error())
                 .addProcessor(ProcessorConfiguration.transport()
-                    .enableJmx(true)
                     .addMailet(MailetConfiguration.builder()
                         .matcher(All.class)
                         .mailet(RemoveMimeHeader.class)
@@ -368,7 +367,6 @@ public class GatewayRemoteDeliveryIntegrationTest {
     public ProcessorConfiguration root() {
         // Custom in memory DNS resolution is not possible combined with InSpamerBackList
         return ProcessorConfiguration.root()
-            .enableJmx(true)
             .addMailet(MailetConfiguration.builder()
                 .matcher(All.class)
                 .mailet(PostmasterAlias.class))
@@ -385,7 +383,6 @@ public class GatewayRemoteDeliveryIntegrationTest {
 
     private ProcessorConfiguration relayOnlyTransport(String gatewayProperty) {
         return ProcessorConfiguration.transport()
-            .enableJmx(true)
             .addMailet(MailetConfiguration.builder()
                 .matcher(All.class)
                 .mailet(RemoveMimeHeader.class)
@@ -406,7 +403,6 @@ public class GatewayRemoteDeliveryIntegrationTest {
 
     private ProcessorConfiguration relayAndLocalDeliveryTransport(String gatewayProperty) {
         return ProcessorConfiguration.transport()
-            .enableJmx(true)
             .addMailet(MailetConfiguration.BCC_STRIPPER)
             .addMailet(MailetConfiguration.builder()
                 .matcher(RecipientIsLocal.class)
@@ -427,7 +423,6 @@ public class GatewayRemoteDeliveryIntegrationTest {
 
     private ProcessorConfiguration directResolutionTransport() {
         return ProcessorConfiguration.transport()
-            .enableJmx(true)
             .addMailet(MailetConfiguration.BCC_STRIPPER)
             .addMailet(MailetConfiguration.builder()
                 .matcher(All.class)
