545da147c233e7d46ec909b471d873e6d76f21f6
benwa <btellier@linagora.com>
2018-01-11 13:47:55 +0700

    JAMES-2285 Use MailQueue contracts to test FileMailQueue

---
+++ server/queue/queue-file/src/test/java/org/apache/james/queue/file/FileMailQueueTest.java
@@ -0,0 +1,155 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.queue.file;
+
+import org.apache.james.queue.api.DelayedMailQueueContract;
+import org.apache.james.queue.api.DelayedManageableMailQueueContract;
+import org.apache.james.queue.api.MailQueue;
+import org.apache.james.queue.api.MailQueueContract;
+import org.apache.james.queue.api.ManageableMailQueue;
+import org.apache.james.queue.api.ManageableMailQueueContract;
+import org.apache.james.queue.api.RawMailQueueItemDecoratorFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.rules.TemporaryFolder;
+
+public class FileMailQueueTest implements MailQueueContract, ManageableMailQueueContract, DelayedMailQueueContract, DelayedManageableMailQueueContract {
+    private static final boolean SYNC = true;
+
+    private TemporaryFolder temporaryFolder = new TemporaryFolder();
+    private FileMailQueue mailQueue;
+
+    @AfterEach
+    void teardown() {
+        temporaryFolder.delete();
+    }
+
+    @BeforeEach
+    public void setUp() throws Exception {
+        temporaryFolder.create();
+        mailQueue = new FileMailQueue(new RawMailQueueItemDecoratorFactory(), temporaryFolder.newFolder(), "test", SYNC);
+    }
+
+    @Override
+    public MailQueue getMailQueue() {
+        return mailQueue;
+    }
+
+    @Override
+    public ManageableMailQueue getManageableMailQueue() {
+        return mailQueue;
+    }
+
+    @Disabled("JAMES-2297 In processing emails are returned by dequeue")
+    public void dequeueShouldNotReturnInProcessingEmails() {
+
+    }
+
+    @Disabled("JAMES-2300 No Order")
+    public void browseShouldReturnOrderElements() {
+
+    }
+
+    @Disabled("JAMES-2300 No Order")
+    public void flushShouldPreserveBrowseOrder() {
+
+    }
+
+    @Disabled("JAMES-2299 No snapshot isolation")
+    public void concurrentClearShouldNotAlterBrowsingWhenDequeueWhileIterating() {
+
+    }
+
+    @Disabled("JAMES-2299 No snapshot isolation")
+    public void concurrentClearShouldNotAlterBrowsingWhenDequeue() {
+
+    }
+
+    @Disabled("JAMES-2299 No snapshot isolation")
+    public void concurrentDequeueShouldNotAlterBrowsing() {
+
+    }
+
+    @Disabled("JAMES-2299 No snapshot isolation")
+    public void concurrentDequeueShouldNotAlterBrowsingWhileIterating() {
+
+    }
+
+    @Disabled("JAMES-2299 No snapshot isolation")
+    public void concurrentDequeueShouldNotAlterBrowsingWhenDequeueWhileIterating() {
+
+    }
+
+    @Disabled("JAMES-2299 No snapshot isolation")
+    public void concurrentEnqueueShouldNotAlterBrowsingWhenDequeueWhileIterating() {
+
+    }
+
+    @Disabled("JAMES-2299 No snapshot isolation")
+    public void concurrentRemoveShouldNotAlterBrowsingWhenDequeueWhileIterating() {
+
+    }
+
+    @Disabled("JAMES-2299 No snapshot isolation")
+    public void concurrentFlushShouldNotAlterBrowsingWhenDequeueWhileIterating() {
+
+    }
+
+    @Disabled("JAMES-2299 No snapshot isolation")
+    public void concurrentFlushShouldNotAlterBrowsing() {
+
+    }
+
+    @Disabled("JAMES-2299 No snapshot isolation")
+    public void concurrentRemoveShouldNotAlterBrowsingWhenDequeue() {
+
+    }
+
+    @Disabled("JAMES-2298 Not supported yet")
+    public void removeBySenderShouldRemoveSpecificEmail() {
+
+    }
+
+    @Disabled("JAMES-2298 Not supported yet")
+    public void removeByNameShouldRemoveSpecificEmail() {
+
+    }
+
+    @Disabled("JAMES-2298 Not supported yet")
+    public void removeByRecipientShouldRemoveSpecificEmail() {
+
+    }
+
+    @Disabled("JAMES-2298 Not supported yet")
+    public void removeByRecipientShouldNotFailWhenQueueIsEmpty() {
+
+    }
+
+    @Disabled("JAMES-2298 Not supported yet")
+    public void removeBySenderShouldNotFailWhenQueueIsEmpty() {
+
+    }
+
+    @Disabled("JAMES-2298 Not supported yet")
+    public void removeByRecipientShouldRemoveSpecificEmailWhenMultipleRecipients() {
+
+    }
+}
