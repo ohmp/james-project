e2b82daec195a7c334a9e0ccb5ba19dfc34015d1
benwa <btellier@linagora.com>
2018-01-18 17:58:54 +0700

    JAMES-2289 MailQueue factories should return created mail queues

--- server/queue/queue-jms/src/test/java/org/apache/james/queue/jms/JMSMailQueueFactoryTest.java
+++ server/queue/queue-jms/src/test/java/org/apache/james/queue/jms/JMSMailQueueFactoryTest.java
@@ -17,45 +17,46 @@
  * under the License.                                           *
  ****************************************************************/
 
-package org.apache.james.queue.memory;
+package org.apache.james.queue.jms;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import javax.jms.ConnectionFactory;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerService;
+import org.apache.james.metrics.api.NoopMetricFactory;
 import org.apache.james.queue.api.MailQueueFactory;
 import org.apache.james.queue.api.MailQueueFactoryContract;
 import org.apache.james.queue.api.RawMailQueueItemDecoratorFactory;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-public class MemoryMailQueueFactoryTest implements MailQueueFactoryContract {
+public class JMSMailQueueFactoryTest implements MailQueueFactoryContract {
 
-    private static final String KEY = "key";
-    private static final String BIS = "keyBis";
+    private final static String QUEUE_NAME = "test";
 
-    private MemoryMailQueueFactory memoryMailQueueFactory;
+    private JMSMailQueueFactory mailQueueFactory;
+
+    private BrokerService broker;
 
     @BeforeEach
-    public void setUp() {
-        memoryMailQueueFactory = new MemoryMailQueueFactory(new RawMailQueueItemDecoratorFactory());
+    public void setUp() throws Exception {
+        broker = BrokerSupplier.createBroker(QUEUE_NAME);
+        broker.start();
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?create=false");
+        RawMailQueueItemDecoratorFactory mailQueueItemDecoratorFactory = new RawMailQueueItemDecoratorFactory();
+        NoopMetricFactory metricFactory = new NoopMetricFactory();
+        mailQueueFactory = new JMSMailQueueFactory(connectionFactory, mailQueueItemDecoratorFactory, metricFactory);
+        mailQueueFactory.setUseJMX(false);
     }
 
-    @Test
-    public void getQueueShouldNotReturnNull() {
-        assertThat(memoryMailQueueFactory.getQueue(KEY)).isNotNull();
-    }
-
-    @Test
-    public void getQueueShouldReturnTwoTimeTheSameResultWhenUsedWithTheSameKey() {
-        assertThat(memoryMailQueueFactory.getQueue(KEY)).isEqualTo(memoryMailQueueFactory.getQueue(KEY));
-    }
-
-    @Test
-    public void getQueueShouldNotReturnTheSameQueueForTwoDifferentNames() {
-        assertThat(memoryMailQueueFactory.getQueue(KEY)).isNotEqualTo(memoryMailQueueFactory.getQueue(BIS));
+    @AfterEach
+    public void tearDown() throws Exception {
+        broker.stop();
+        mailQueueFactory.destroy();
     }
 
     @Override
     public MailQueueFactory getMailQueueFactory() {
-        return memoryMailQueueFactory;
+        return mailQueueFactory;
     }
 }
