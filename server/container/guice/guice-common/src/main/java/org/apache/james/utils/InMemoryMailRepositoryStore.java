e092786519c0c4a3ef2f683c2f58b35ee2fe66c7
benwa <btellier@linagora.com>
2018-01-24 17:14:58 +0700

    JAMES-2291 Fix a typo in InMemoryMailRepositoryStore

--- server/container/guice/guice-common/src/main/java/org/apache/james/utils/InMemoryMailRepositoryStore.java
+++ server/container/guice/guice-common/src/main/java/org/apache/james/utils/InMemoryMailRepositoryStore.java
@@ -116,7 +116,7 @@ public class InMemoryMailRepositoryStore implements MailRepositoryStore, Configu
             return mailRepository;
         }
         mailRepository = retrieveMailRepository(destination);
-        mailRepository = initialiseNewRepository(mailRepository, createRepositoryCombinedConfig(destination));
+        mailRepository = initializeNewRepository(mailRepository, createRepositoryCombinedConfig(destination));
         destinationToRepositoryAssociations.putIfAbsent(destination.url, mailRepository);
         return mailRepository;
     }
@@ -155,7 +155,7 @@ public class InMemoryMailRepositoryStore implements MailRepositoryStore, Configu
         return config;
     }
 
-    private MailRepository initialiseNewRepository(MailRepository mailRepository, CombinedConfiguration config) throws MailRepositoryStoreException {
+    private MailRepository initializeNewRepository(MailRepository mailRepository, CombinedConfiguration config) throws MailRepositoryStoreException {
         try {
             if (mailRepository instanceof Configurable) {
                 ((Configurable) mailRepository).configure(config);
