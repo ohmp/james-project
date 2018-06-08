c5ea121c79698cb4bf011c8432a5c48021d8f6af
benwa <btellier@linagora.com>
2018-06-08 11:54:48 +0700

    ISSUE-674 Provide MailRepositoryUrlStore cassandra implementation

---
+++ server/data/data-cassandra/src/main/java/org/apache/james/mailrepository/cassandra/UrlsDao.java
@@ -0,0 +1,81 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mailrepository.cassandra;
+
+import static com.datastax.driver.core.querybuilder.QueryBuilder.bindMarker;
+import static com.datastax.driver.core.querybuilder.QueryBuilder.insertInto;
+import static com.datastax.driver.core.querybuilder.QueryBuilder.select;
+import static org.apache.james.mailrepository.cassandra.UrlsTable.TABLE_NAME;
+import static org.apache.james.mailrepository.cassandra.UrlsTable.URL;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+import javax.inject.Inject;
+
+import org.apache.james.backends.cassandra.utils.CassandraAsyncExecutor;
+import org.apache.james.backends.cassandra.utils.CassandraUtils;
+import org.apache.james.mailrepository.api.MailRepositoryUrl;
+
+import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
+public class UrlsDao {
+    private final CassandraAsyncExecutor executor;
+    private final PreparedStatement insert;
+    private final PreparedStatement selectAll;
+    private final CassandraUtils cassandraUtils;
+
+    @Inject
+    public UrlsDao(Session session, CassandraUtils cassandraUtils) {
+        this.executor = new CassandraAsyncExecutor(session);
+
+        insert = prepareInsert(session);
+        selectAll = prepareSelectAll(session);
+        this.cassandraUtils = cassandraUtils;
+    }
+
+    private PreparedStatement prepareSelectAll(Session session) {
+        return session.prepare(select(URL)
+            .from(TABLE_NAME));
+    }
+
+    private PreparedStatement prepareInsert(Session session) {
+        return session.prepare(insertInto(TABLE_NAME)
+            .value(URL, bindMarker(URL)));
+    }
+
+    public CompletableFuture<Void> addUrl(MailRepositoryUrl url) {
+        return executor.executeVoid(
+            insert.bind()
+            .setString(URL, url.getValue()));
+    }
+
+    public CompletableFuture<Stream<MailRepositoryUrl>> retrieveusedUrl() {
+        return executor.execute(selectAll.bind())
+            .thenApply(resultSet -> cassandraUtils.convertToStream(resultSet)
+                .map(this::toUrl));
+    }
+
+    private MailRepositoryUrl toUrl(Row row) {
+        return new MailRepositoryUrl(row.getString(URL));
+    }
+}
