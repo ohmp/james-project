0334742b6010eff40441a20ce17e8c12639c9c5f
benwa <btellier@linagora.com>
2018-06-08 11:29:07 +0700

    ISSUE-674 Provide MailRepositoryUrlStore API, contract and memory implementation

---
+++ server/data/data-api/src/test/java/org/apache/james/mailrepository/api/MailRepositoryUrlStoreContract.java
@@ -0,0 +1,58 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+
+package org.apache.james.mailrepository.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public interface MailRepositoryUrlStoreContract {
+    MailRepositoryUrl URL_1 = new MailRepositoryUrl("proto://var/mail/toto");
+    MailRepositoryUrl URL_2 = new MailRepositoryUrl("proto://var/mail/tata");
+
+    @Test
+    default void shouldBeEmptyByDefault(MailRepositoryUrlStore store) {
+        assertThat(store.retrieveusedUrl()).isEmpty();
+    }
+
+    @Test
+    default void retrieveusedUrlShouldReturnAddedUrl(MailRepositoryUrlStore store) {
+        store.addUrl(URL_1);
+
+        assertThat(store.retrieveusedUrl()).containsOnly(URL_1);
+    }
+
+    @Test
+    default void retrieveusedUrlShouldNotReturnDuplicates(MailRepositoryUrlStore store) {
+        store.addUrl(URL_1);
+        store.addUrl(URL_1);
+
+        assertThat(store.retrieveusedUrl()).containsOnly(URL_1);
+    }
+
+    @Test
+    default void retrieveusedUrlShouldReturnAddedUrls(MailRepositoryUrlStore store) {
+        store.addUrl(URL_1);
+        store.addUrl(URL_2);
+
+        assertThat(store.retrieveusedUrl()).containsOnly(URL_1, URL_2);
+    }
+
+}
