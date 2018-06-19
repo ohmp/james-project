bc8fd2b8ce11733f6daa0ab39e6ce031b6aa0e93
benwa <btellier@linagora.com>
2018-06-19 07:56:56 +0700

    fixup! JAMES-2414 provide a default getAttachment implementation to have homogeneous MailboxMessage behavior at runtime

--- mailbox/hbase/src/main/java/org/apache/james/mailbox/hbase/mail/HBaseMailboxMessage.java
+++ mailbox/hbase/src/main/java/org/apache/james/mailbox/hbase/mail/HBaseMailboxMessage.java
@@ -34,7 +34,6 @@ import java.util.List;
 
 import javax.mail.Flags;
 
-import org.apache.commons.lang.NotImplementedException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.james.mailbox.MessageUid;
 import org.apache.james.mailbox.exception.MailboxException;
