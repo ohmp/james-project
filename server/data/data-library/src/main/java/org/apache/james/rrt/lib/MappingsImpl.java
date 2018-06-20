1bad15b732eaba9936b86ed3bf66bf69406eb62c
benwa <btellier@linagora.com>
2018-06-20 09:13:40 +0700

    fixup! JAMES-2149 Create domain mappings via webadmin

--- server/data/data-library/src/main/java/org/apache/james/rrt/lib/MappingsImpl.java
+++ server/data/data-library/src/main/java/org/apache/james/rrt/lib/MappingsImpl.java
@@ -26,7 +26,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Optional;
 import java.util.StringTokenizer;
 import java.util.function.Predicate;
