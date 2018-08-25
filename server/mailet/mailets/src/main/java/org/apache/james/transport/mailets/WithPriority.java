09ddfb549c4d5823509d8ff273da4a7e971990e2
ilker yıldırım <ilker.ceng@gmail.com>
2018-08-25 12:09:19 +0300

    with priority configuration for priority mailets

--- server/mailet/mailets/src/main/java/org/apache/james/transport/mailets/WithPriority.java
+++ server/mailet/mailets/src/main/java/org/apache/james/transport/mailets/WithPriority.java
@@ -49,12 +49,12 @@ public class WithPriority extends GenericMailet {
 
     @Override
     public void init() throws MessagingException {
-        Integer priorityRaw = Optional.ofNullable(getInitParameter("priority", null))
-            .map(Integer::valueOf)
-            .orElseThrow(() -> new IllegalArgumentException("'priority' init parameter is compulsory"));
+        priority = Optional.ofNullable(getInitParameter("value", null))
+                .map(Integer::valueOf)
+                .orElseThrow(() -> new IllegalArgumentException("'value' init parameter is compulsory"));
 
-        if (priorityRaw < 0 || priorityRaw > 9) {
-            throw new IllegalArgumentException("Invalid priority: Priority should be from 0 to 9");
+        if (priority < 0 || priority > 9) {
+            throw new IllegalArgumentException("Invalid priority value: Value for Priority should be from 0 to 9");
         }
         priority = new Attribute(MailPrioritySupport.MAIL_PRIORITY, AttributeValue.of(priorityRaw));
     }
