415911c1007941d2f77e7922ed6a97ddcf33829e
Benoit Tellier <btellier@linagora.com>
2018-09-04 10:02:35 +0700

    fixup! JAMES-2420 Add tests to DLP rule store

--- server/data/data-api/src/test/java/org/apache/james/dlp/api/DLPConfigurationStoreContract.java
+++ server/data/data-api/src/test/java/org/apache/james/dlp/api/DLPConfigurationStoreContract.java
@@ -109,7 +109,7 @@ public interface DLPConfigurationStoreContract {
     }
 
     @Test
-    default void storeShouldRejectDuplicates(DLPConfigurationStore dlpConfigurationStore) {
+    default void storeShouldRejectIdDuplicates(DLPConfigurationStore dlpConfigurationStore) {
         assertThatThrownBy(() -> dlpConfigurationStore.store(Domain.LOCALHOST, RULE, RULE))
             .isInstanceOf(IllegalArgumentException.class);
     }
