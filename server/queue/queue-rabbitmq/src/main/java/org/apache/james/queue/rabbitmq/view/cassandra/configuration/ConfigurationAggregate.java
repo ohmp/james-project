7011f762ee119afa7730fccc0ea34e2947307255
Benoit Tellier <btellier@linagora.com>
2018-09-26 17:05:47 +0700

    Avoid data race and redefine ES system behaviour

--- server/queue/queue-rabbitmq/src/main/java/org/apache/james/queue/rabbitmq/view/cassandra/configuration/ConfigurationAggregate.java
+++ server/queue/queue-rabbitmq/src/main/java/org/apache/james/queue/rabbitmq/view/cassandra/configuration/ConfigurationAggregate.java
@@ -25,18 +25,13 @@ import java.util.Optional;
 import org.apache.james.eventsourcing.AggregateId;
 import org.apache.james.eventsourcing.Event;
 import org.apache.james.eventsourcing.eventstore.History;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
 
-
 class ConfigurationAggregate {
 
     private static class State {
-
-        private static final Logger LOGGER = LoggerFactory.getLogger(State.class);
 
         private static State initial() {
             return new State(Optional.empty());
@@ -49,18 +44,8 @@ class ConfigurationAggregate {
             this.maybeConfiguration = maybeConfiguration;
         }
 
-        boolean set(CassandraMailQueueViewConfiguration configuration) {
-            validateConfiguration(configuration);
-            boolean isSame = maybeConfiguration
-                .map(currentConfiguration -> currentConfiguration.equals(configuration))
-                .orElse(false);
-
-            if (!isSame) {
-                maybeConfiguration = Optional.of(configuration);
-                return true;
-            } else {
-                return false;
-            }
+        void set(CassandraMailQueueViewConfiguration configuration) {
+            maybeConfiguration = Optional.of(configuration);
         }
 
         private void validateConfiguration(CassandraMailQueueViewConfiguration configurationUpdate) {
@@ -101,24 +86,26 @@ class ConfigurationAggregate {
     }
 
     List<? extends Event> applyConfiguration(CassandraMailQueueViewConfiguration configuration) {
-        ConfigurationEdited newEvent = new ConfigurationEdited(
-            history.getNextEventId(),
-            configuration);
+        state.validateConfiguration(configuration);
 
-        return apply(newEvent);
+        if (state.maybeConfiguration.map(configuration::equals).orElse(false)) {
+            return EMPTY_EVENTS;
+        }
+
+        return ImmutableList.of(new ConfigurationEdited(
+            history.getNextEventId(),
+            configuration));
     }
 
     Optional<CassandraMailQueueViewConfiguration> getCurrentConfiguration() {
         return state.maybeConfiguration;
     }
 
-    private List<? extends Event> apply(Event event) {
+    private void apply(Event event) {
         if (event instanceof ConfigurationEdited) {
-            if (state.set(((ConfigurationEdited) event).getConfiguration())) {
-                return ImmutableList.of(event);
-            }
-        }
+            ConfigurationEdited configurationEdited = (ConfigurationEdited) event;
 
-        return EMPTY_EVENTS;
+            state.set(configurationEdited.getConfiguration());
+        }
     }
 }
