ba4cb9b1f70129b7e92e74c6c6e9ea5f0801c20c
Benoit Tellier <btellier@linagora.com>
2018-10-03 15:08:03 +0700

    Migrate JMAP integration tests to JUNIT 5: CassandraBulkOperationTest

--- server/protocols/jmap-integration-testing/cassandra-jmap-integration-testing/src/test/java/org/apache/james/jmap/cassandra/CassandraBulkOperationTest.java
+++ server/protocols/jmap-integration-testing/cassandra-jmap-integration-testing/src/test/java/org/apache/james/jmap/cassandra/CassandraBulkOperationTest.java
@@ -20,6 +20,7 @@
 package org.apache.james.jmap.cassandra;
 
 import static io.restassured.RestAssured.given;
+import static org.apache.james.CassandraJamesServerMain.ALL_BUT_JMX_CASSANDRA_MODULE;
 import static org.apache.james.jmap.JmapURIBuilder.baseUri;
 import static org.apache.james.jmap.TestingConstants.ARGUMENTS;
 import static org.apache.james.jmap.TestingConstants.DOMAIN;
@@ -35,9 +36,10 @@ import java.util.Date;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import org.apache.james.CassandraJmapTestRule;
-import org.apache.james.DockerCassandraRule;
+import org.apache.james.CassandraExtension;
+import org.apache.james.EmbeddedElasticSearchExtension;
 import org.apache.james.GuiceJamesServer;
+import org.apache.james.JamesServerExtension;
 import org.apache.james.backends.cassandra.init.configuration.CassandraConfiguration;
 import org.apache.james.jmap.HttpJmapAuthentication;
 import org.apache.james.jmap.api.access.AccessToken;
@@ -47,109 +49,109 @@ import org.apache.james.mailbox.exception.MailboxException;
 import org.apache.james.mailbox.model.MailboxPath;
 import org.apache.james.mime4j.dom.Message;
 import org.apache.james.modules.MailboxProbeImpl;
+import org.apache.james.modules.TestJMAPServerModule;
 import org.apache.james.utils.DataProbeImpl;
 import org.apache.james.utils.JmapGuiceProbe;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 import io.restassured.RestAssured;
 import io.restassured.parsing.Parser;
 
-public class CassandraBulkOperationTest {
+class CassandraBulkOperationTest {
     private static final Integer NUMBER_OF_MAIL_TO_CREATE = 250;
-
-    @ClassRule
-    public static DockerCassandraRule cassandra =  new DockerCassandraRule();
-
-    @Rule
-    public CassandraJmapTestRule rule = CassandraJmapTestRule.defaultTestRule();
-
     private static final String USERNAME = "username@" + DOMAIN;
     private static final MailboxPath TRASH_PATH = MailboxPath.forUser(USERNAME, DefaultMailboxes.TRASH);
     private static final String PASSWORD = "password";
 
-    private GuiceJamesServer jmapServer;
-
-    @Before
-    public void setup() {
+    @BeforeEach
+    void setup(GuiceJamesServer server) throws Exception {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.defaultParser = Parser.JSON;
+
+        RestAssured.requestSpecification = jmapRequestSpecBuilder
+            .setPort(server.getProbe(JmapGuiceProbe.class).getJmapPort())
+            .build();
+
+        server.getProbe(DataProbeImpl.class)
+            .fluent()
+            .addDomain(DOMAIN)
+            .addUser(USERNAME, PASSWORD);
+
+        server.getProbe(MailboxProbeImpl.class).createMailbox(TRASH_PATH);
     }
 
-    @After
-    public void teardown() {
-        if (jmapServer != null) {
-            jmapServer.stop();
+    @Nested
+    class SmallChunkSize {
+        @RegisterExtension
+        JamesServerExtension testExtension = serverExtensionWithChunkSize(85);
+
+        @Test
+        void setMessagesShouldWorkForHugeNumberOfEmailsToTrashWhenChunksConfigurationAreLowEnough(GuiceJamesServer server) {
+            String mailIds = provistionMails(server, NUMBER_OF_MAIL_TO_CREATE);
+
+            AccessToken accessToken = HttpJmapAuthentication.authenticateJamesUser(baseUri(server), USERNAME, PASSWORD);
+            given()
+                .header("Authorization", accessToken.serialize())
+                .body("[[\"setMessages\", {\"destroy\": [" + mailIds + "]}, \"#0\"]]")
+            .when()
+                .post("/jmap")
+            .then()
+                .statusCode(200)
+                .log().ifValidationFails()
+                .body(NAME, equalTo("messagesSet"))
+                .body(ARGUMENTS + ".destroyed", hasSize(NUMBER_OF_MAIL_TO_CREATE));
         }
     }
 
-    @Test
-    public void setMessagesShouldWorkForHugeNumberOfEmailsToTrashWhenChunksConfigurationAreLowEnough() throws Exception {
-        jmapServer = createServerWithExpungeChunkSize(85);
-        String mailIds = provistionMails(NUMBER_OF_MAIL_TO_CREATE);
+    @Nested
+    class TooBigChunkSize {
+        @RegisterExtension
+        JamesServerExtension testExtension = serverExtensionWithChunkSize(NUMBER_OF_MAIL_TO_CREATE);
 
-        AccessToken accessToken = HttpJmapAuthentication.authenticateJamesUser(baseUri(jmapServer), USERNAME, PASSWORD);
-        given()
-            .header("Authorization", accessToken.serialize())
-            .body("[[\"setMessages\", {\"destroy\": [" + mailIds + "]}, \"#0\"]]")
-        .when()
-            .post("/jmap")
-        .then()
-            .statusCode(200)
-            .log().ifValidationFails()
-            .body(NAME, equalTo("messagesSet"))
-            .body(ARGUMENTS + ".destroyed", hasSize(NUMBER_OF_MAIL_TO_CREATE));
+        @Test
+        void setMessagesShouldFailForHugeNumberOfEmailsToTrashWhenChunksConfigurationAreTooBig(GuiceJamesServer server) {
+            String mailIds = provistionMails(server, NUMBER_OF_MAIL_TO_CREATE);
+
+            AccessToken accessToken = HttpJmapAuthentication.authenticateJamesUser(baseUri(server), USERNAME, PASSWORD);
+            given()
+                .header("Authorization", accessToken.serialize())
+                .body("[[\"setMessages\", {\"destroy\": [" + mailIds + "]}, \"#0\"]]")
+            .when()
+                .post("/jmap")
+            .then()
+                .statusCode(400);
+        }
     }
 
-    @Test
-    public void setMessagesShouldFailForHugeNumberOfEmailsToTrashWhenChunksConfigurationAreTooBig() throws Exception {
-        jmapServer = createServerWithExpungeChunkSize(NUMBER_OF_MAIL_TO_CREATE);
-        String mailIds = provistionMails(NUMBER_OF_MAIL_TO_CREATE);
-
-        AccessToken accessToken = HttpJmapAuthentication.authenticateJamesUser(baseUri(jmapServer), USERNAME, PASSWORD);
-        given()
-            .header("Authorization", accessToken.serialize())
-            .body("[[\"setMessages\", {\"destroy\": [" + mailIds + "]}, \"#0\"]]")
-        .when()
-            .post("/jmap")
-        .then()
-            .statusCode(400);
+    private JamesServerExtension serverExtensionWithChunkSize(int chunkSize) {
+        return JamesServerExtension.builder()
+            .extension(new EmbeddedElasticSearchExtension())
+            .extension(new CassandraExtension())
+            .server(configuration -> GuiceJamesServer.forConfiguration(configuration)
+                .combineWith(ALL_BUT_JMX_CASSANDRA_MODULE)
+                .overrideWith(TestJMAPServerModule.DEFAULT)
+                .overrideWith(binder -> binder.bind(CassandraConfiguration.class)
+                    .toInstance(
+                        CassandraConfiguration.builder()
+                            .expungeChunkSize(chunkSize)
+                            .build())))
+            .build();
     }
 
-    private String provistionMails(int count) {
+    private String provistionMails(GuiceJamesServer server, int count) {
         return IntStream.rangeClosed(1, count)
-            .mapToObj(i -> appendOneMessageToTrash())
+            .mapToObj(i -> appendOneMessageToTrash(server))
             .map(this::quote)
             .collect(Collectors.joining(","));
     }
 
-    private GuiceJamesServer createServerWithExpungeChunkSize(int expungeChunkSize) throws Exception {
-        GuiceJamesServer jmapServer = rule.jmapServer(cassandra.getModule(),
-            binder -> binder.bind(CassandraConfiguration.class)
-                .toInstance(
-                    CassandraConfiguration.builder()
-                        .expungeChunkSize(expungeChunkSize)
-                        .build()));
-        jmapServer.start();
-        RestAssured.requestSpecification = jmapRequestSpecBuilder
-            .setPort(jmapServer.getProbe(JmapGuiceProbe.class).getJmapPort())
-            .build();
-
-        jmapServer.getProbe(DataProbeImpl.class)
-            .fluent()
-            .addDomain(DOMAIN)
-            .addUser(USERNAME, PASSWORD);
-        jmapServer.getProbe(MailboxProbeImpl.class).createMailbox(TRASH_PATH);
-        return jmapServer;
-    }
-
-    private String appendOneMessageToTrash() {
+    private String appendOneMessageToTrash(GuiceJamesServer server) {
         ZonedDateTime dateTime = ZonedDateTime.parse("2014-10-30T14:12:00Z");
         try {
-            return jmapServer.getProbe(MailboxProbeImpl.class)
+            return server.getProbe(MailboxProbeImpl.class)
                 .appendMessage(USERNAME, TRASH_PATH,
                     AppendCommand.builder()
                         .build(Message.Builder
@@ -167,5 +169,4 @@ public class CassandraBulkOperationTest {
     private String quote(String id) {
         return '"' + id + '"';
     }
-
 }
