95448017fb52d185358c1c0d0ac912c89fa9b301
Benoit Tellier <btellier@linagora.com>
2018-12-03 16:56:39 +0700

    JAMES-2525 Experiment other HTTP drivers

--- server/blob/blob-objectstorage/src/main/java/org/apache/james/blob/objectstorage/swift/SwiftKeystone2ObjectStorage.java
+++ server/blob/blob-objectstorage/src/main/java/org/apache/james/blob/objectstorage/swift/SwiftKeystone2ObjectStorage.java
@@ -27,6 +27,7 @@ import java.util.function.Supplier;
 import org.apache.james.blob.objectstorage.ObjectStorageBlobsDAOBuilder;
 import org.jclouds.ContextBuilder;
 import org.jclouds.blobstore.BlobStore;
+import org.jclouds.http.apachehc.config.ApacheHCHttpCommandExecutorServiceModule;
 import org.jclouds.logging.slf4j.config.SLF4JLoggingModule;
 import org.jclouds.openstack.keystone.config.KeystoneProperties;
 import org.jclouds.openstack.swift.v1.blobstore.RegionScopedBlobStoreContext;
@@ -41,7 +42,7 @@ public class SwiftKeystone2ObjectStorage {
     public static final String AUTH_API_NAME = "keystone2";
 
     private static final Iterable<Module> JCLOUDS_MODULES =
-        ImmutableSet.of(new SLF4JLoggingModule());
+        ImmutableSet.of(new SLF4JLoggingModule(), new ApacheHCHttpCommandExecutorServiceModule());
 
     public static ObjectStorageBlobsDAOBuilder.RequireContainerName daoBuilder(Configuration testConfig) {
         return ObjectStorageBlobsDAOBuilder.forBlobStore(new BlobStoreBuilder(testConfig));
