cc536d73fd6f7ca7356054066f7150b2db481bb3
Benoit Tellier <btellier@linagora.com>
2018-12-13 09:12:56 +0700

    fixup! MAILBOX-364 In VM implementation for EventBus

--- mailbox/event/event-memory/src/test/java/org/apache/james/mailbox/events/MemoryEventBusTest.java
+++ mailbox/event/event-memory/src/test/java/org/apache/james/mailbox/events/MemoryEventBusTest.java
@@ -19,7 +19,7 @@
 
 package org.apache.james.mailbox.events;
 
-import org.apache.james.mailbox.events.delivery.EventDeliveryImpl;
+import org.apache.james.mailbox.events.delivery.InVmEventDelivery;
 import org.apache.james.metrics.api.NoopMetricFactory;
 import org.junit.jupiter.api.BeforeEach;
 
@@ -29,7 +29,7 @@ public class MemoryEventBusTest implements EventBusContract {
     @BeforeEach
     void setUp() {
         eventBus = new MemoryEventBus(
-            new EventDeliveryImpl(
+            new InVmEventDelivery(
                 new NoopMetricFactory()));
     }
 
