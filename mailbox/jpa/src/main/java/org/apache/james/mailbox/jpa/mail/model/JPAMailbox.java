de6a53853094e580865391fdd368478d03884cd0
Benoit Tellier <btellier@linagora.com>
2019-04-04 11:37:50 +0700

    JAMES-2723 Merge SimpleMailbox and Mailbox
    Mailbox then becomes a standard POJO

--- mailbox/jpa/src/main/java/org/apache/james/mailbox/jpa/mail/model/JPAMailbox.java
+++ mailbox/jpa/src/main/java/org/apache/james/mailbox/jpa/mail/model/JPAMailbox.java
@@ -27,7 +27,6 @@ import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 
-import org.apache.james.mailbox.SimpleMailbox;
 import org.apache.james.mailbox.jpa.JPAId;
 import org.apache.james.mailbox.model.Mailbox;
 import org.apache.james.mailbox.model.MailboxId;
@@ -138,7 +137,7 @@ public class JPAMailbox {
     }
 
     public Mailbox toMailbox() {
-        return new SimpleMailbox(generateAssociatedPath(), uidValidity, new JPAId(mailboxId));
+        return new Mailbox(generateAssociatedPath(), uidValidity, new JPAId(mailboxId));
     }
 
     public MailboxPath generateAssociatedPath() {
