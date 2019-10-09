819d9e1e0abae6da7ffa40fd6f8ac6958cf661bb
Benoit Tellier <btellier@linagora.com>
2019-10-09 13:57:42 +0700

    JAMES-2915 Remove no longer maintained (and failing!) cassandra backend support for Spring

--- server/app/src/test/java/org/apache/james/app/spring/JamesSpringContextTest.java
+++ server/app/src/test/java/org/apache/james/app/spring/JamesSpringContextTest.java
@@ -24,7 +24,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.apache.james.container.spring.context.JamesServerApplicationContext;
 import org.apache.james.mailbox.events.InVMEventBus;
 import org.apache.james.mailbox.lucene.search.LuceneMessageSearchIndex;
-import org.apache.james.mailbox.store.quota.ListeningCurrentQuotaUpdater;
+import org.apache.james.mailbox.store.event.MailboxAnnotationListener;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -50,6 +50,6 @@ public class JamesSpringContextTest {
         InVMEventBus eventBus = context.getBean(InVMEventBus.class);
 
         assertThat(eventBus.registeredGroups())
-            .containsExactlyInAnyOrder(ListeningCurrentQuotaUpdater.GROUP, LuceneMessageSearchIndex.GROUP);
+            .containsExactlyInAnyOrder(MailboxAnnotationListener.GROUP, LuceneMessageSearchIndex.GROUP);
     }
 }
