b2a4d1c6917d6a69e218d1717df8339cd04dee70
Benoit Tellier <btellier@linagora.com>
2019-11-20 10:18:40 +0700

    fixup! [Refactoring] JPAModSeqProvider: transaction upon reads are not required

--- mailbox/jpa/src/main/java/org/apache/james/mailbox/jpa/mail/JPAModSeqProvider.java
+++ mailbox/jpa/src/main/java/org/apache/james/mailbox/jpa/mail/JPAModSeqProvider.java
@@ -75,7 +75,7 @@ public class JPAModSeqProvider implements ModSeqProvider {
             if (manager != null && manager.getTransaction().isActive()) {
                 manager.getTransaction().rollback();
             }
-            throw new MailboxException("Unable to save highest mod-sequence for mailbox " + mailboxId, e);
+            throw new MailboxException("Unable to save highest mod-sequence for mailbox " + mailboxId.serialize(), e);
         } finally {
             if (manager != null) {
                 manager.close();
@@ -92,7 +92,7 @@ public class JPAModSeqProvider implements ModSeqProvider {
                 .getSingleResult();
             return highest;
         } catch (PersistenceException e) {
-            throw new MailboxException("Unable to get highest mod-sequence for mailbox " + mailboxId, e);
+            throw new MailboxException("Unable to get highest mod-sequence for mailbox " + mailboxId.serialize(), e);
         } finally {
             if (manager != null) {
                 manager.close();
