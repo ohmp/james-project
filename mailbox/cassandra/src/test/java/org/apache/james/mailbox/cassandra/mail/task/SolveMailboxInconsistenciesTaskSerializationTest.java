7fcd125300cbd6c8c7c11fa1c00a3d2bd7e58df8
Benoit Tellier <btellier@linagora.com>
2020-02-14 10:05:37 +0700

    fixup! JAMES-3058 Wrap mailbox inconsistency solving in a task

--- mailbox/cassandra/src/test/java/org/apache/james/mailbox/cassandra/mail/task/SolveMailboxInconsistenciesTaskSerializationTest.java
+++ mailbox/cassandra/src/test/java/org/apache/james/mailbox/cassandra/mail/task/SolveMailboxInconsistenciesTaskSerializationTest.java
@@ -43,10 +43,29 @@ class SolveMailboxInconsistenciesTaskSerializationTest {
     private static final SolveMailboxInconsistenciesService SERVICE = mock(SolveMailboxInconsistenciesService.class);
     private static final SolveMailboxInconsistenciesTask TASK = new SolveMailboxInconsistenciesTask(SERVICE);
     private static final String SERIALIZED_TASK = "{\"type\": \"solve-mailbox-inconsistencies\"}";
-    private static final ConflictingEntry CONFLICTING_ENTRY = new ConflictingEntry(MAILBOX_PATH, MAILBOX_ID, MAILBOX_PATH_2, MAILBOX_ID);
+    private static final ConflictingEntry CONFLICTING_ENTRY = ConflictingEntry.builder()
+        .mailboxDaoEntry(MAILBOX_PATH, MAILBOX_ID)
+        .mailboxPathDaoEntry(MAILBOX_PATH_2, MAILBOX_ID);
     private static final ImmutableList<ConflictingEntry> CONFLICTING_ENTRIES = ImmutableList.of(CONFLICTING_ENTRY);
     private static final SolveMailboxInconsistenciesTask.Details DETAILS = new SolveMailboxInconsistenciesTask.Details(TIMESTAMP, 0, 1, 2, CONFLICTING_ENTRIES, 3);
-    private static final String SERIALIZED_ADDITIONAL_INFORMATION = "{\"type\":\"solve-mailbox-inconsistencies\",\"processedMailboxEntries\":0,\"processedMailboxPathEntries\":1,\"fixedInconsistencies\":2,\"conflictingEntries\":[{\"mailboxPathAsString\":\"#private:user:mailboxName\",\"mailboxIdAsString\":\"464765a0-e4e7-11e4-aba4-710c1de3782b\",\"pathRegistrationPathAsString\":\"#private:user:mailboxName2\",\"pathRegistrationIdAsString\":\"464765a0-e4e7-11e4-aba4-710c1de3782b\"}],\"errors\":3,\"timestamp\":\"2018-11-13T12:00:55Z\"}";
+    private static final String SERIALIZED_ADDITIONAL_INFORMATION = "{" +
+        "  \"type\":\"solve-mailbox-inconsistencies\"," +
+        "  \"processedMailboxEntries\":0," +
+        "  \"processedMailboxPathEntries\":1," +
+        "  \"fixedInconsistencies\":2," +
+        "  \"conflictingEntries\":[{" +
+        "    \"mailboxDaoEntry\":{" +
+        "      \"mailboxPath\":\"#private:user:mailboxName\"," +
+        "      \"mailboxId\":\"464765a0-e4e7-11e4-aba4-710c1de3782b\"" +
+        "    }," +
+        "    \"mailboxPathDaoEntry\":{" +
+        "      \"mailboxPath\":\"#private:user:mailboxName2\"," +
+        "      \"mailboxId\":\"464765a0-e4e7-11e4-aba4-710c1de3782b\"" +
+        "    }" +
+        "  }]," +
+        "  \"errors\":3," +
+        "  \"timestamp\":\"2018-11-13T12:00:55Z\"" +
+        "}";
 
     @Test
     void taskShouldBeSerializable() throws Exception {
