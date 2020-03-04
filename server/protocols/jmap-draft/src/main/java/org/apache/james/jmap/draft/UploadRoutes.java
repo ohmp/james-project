bd81ac52f41719e7207fd14860a534470806f71e
Benoit Tellier <btellier@linagora.com>
2020-03-04 11:11:00 +0700

    Upload routes

---
+++ server/protocols/jmap-draft/src/main/java/org/apache/james/jmap/draft/UploadRoutes.java
@@ -0,0 +1,171 @@
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
+package org.apache.james.jmap.draft;
+
+import static javax.servlet.http.HttpServletResponse.SC_CREATED;
+import static org.apache.james.jmap.HttpConstants.CONTENT_TYPE;
+import static org.apache.james.jmap.HttpConstants.JSON_CONTENT_TYPE_UTF8;
+import static org.apache.james.jmap.HttpConstants.SC_BAD_REQUEST;
+import static org.apache.james.jmap.HttpConstants.SC_INTERNAL_SERVER_ERROR;
+import static org.apache.james.jmap.draft.JMAPUrls.JMAP;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.inject.Inject;
+
+import org.apache.james.jmap.HttpConstants;
+import org.apache.james.jmap.JMAPRoutes;
+import org.apache.james.jmap.draft.exceptions.BadRequestException;
+import org.apache.james.jmap.draft.exceptions.InternalErrorException;
+import org.apache.james.jmap.draft.model.UploadResponse;
+import org.apache.james.mailbox.AttachmentManager;
+import org.apache.james.mailbox.MailboxSession;
+import org.apache.james.mailbox.exception.MailboxException;
+import org.apache.james.mailbox.model.Attachment;
+import org.apache.james.metrics.api.MetricFactory;
+import org.apache.james.util.ReactorUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
+import com.google.common.io.ByteStreams;
+
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+import reactor.netty.http.server.HttpServerRequest;
+import reactor.netty.http.server.HttpServerResponse;
+import reactor.netty.http.server.HttpServerRoutes;
+
+public class UploadRoutes implements JMAPRoutes {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UploadRoutes.class);
+
+    static class CancelledUploadException extends RuntimeException {
+
+    }
+
+    private final MetricFactory metricFactory;
+    private final AuthenticationReactiveFilter authenticationReactiveFilter;
+    private final AttachmentManager attachmentManager;
+    private final ObjectMapper objectMapper;
+
+    @Inject
+    private UploadRoutes(MetricFactory metricFactory, AuthenticationReactiveFilter authenticationReactiveFilter, AttachmentManager attachmentManager, ObjectMapper objectMapper) {
+        this.metricFactory = metricFactory;
+        this.authenticationReactiveFilter = authenticationReactiveFilter;
+        this.attachmentManager = attachmentManager;
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public HttpServerRoutes define(HttpServerRoutes builder) {
+        return builder.post(JMAP, this::post)
+            .options(JMAP, CORS_CONTROL);
+    }
+
+    private Mono<Void> post(HttpServerRequest request, HttpServerResponse response)  {
+        String contentType = request.requestHeaders().get(CONTENT_TYPE);
+        if (Strings.isNullOrEmpty(contentType)) {
+            return response.status(HttpConstants.SC_BAD_REQUEST).send();
+        } else {
+            return authenticationReactiveFilter.authenticate(request)
+                .flatMap(session -> post(request, response, contentType, session))
+                .onErrorResume(CancelledUploadException.class, e -> handleCanceledUpload(response, e))
+                .onErrorResume(BadRequestException.class, e -> handleBadRequest(response, e))
+                .onErrorResume(InternalErrorException.class, e -> handleInternalError(response, e))
+                .subscribeOn(Schedulers.elastic());
+        }
+    }
+
+    private Mono<Void> post(HttpServerRequest request, HttpServerResponse response, String contentType, MailboxSession session) {
+        InputStream content = ReactorUtils.toInputStream(request.receive().asByteBuffer());
+        return metricFactory.runPublishingTimerMetric("JMAP-upload-post",
+            handle(contentType, content, session, response));
+    }
+
+    public Mono<Void> handle(String contentType, InputStream content, MailboxSession mailboxSession, HttpServerResponse response) {
+        return uploadContent(contentType, content, mailboxSession)
+            .flatMap(storedContent -> {
+                try {
+                    return response.header(CONTENT_TYPE, JSON_CONTENT_TYPE_UTF8)
+                        .status(SC_CREATED)
+                        .sendString(Mono.just(objectMapper.writeValueAsString(storedContent)))
+                        .then();
+                } catch (JsonProcessingException e) {
+                    throw new InternalErrorException("Error serializing upload response", e);
+                }
+            });
+    }
+
+    private Mono<UploadResponse> uploadContent(String contentType, InputStream inputStream, MailboxSession session) {
+        return toBytesArray(inputStream)
+            .map(bytes -> Attachment.builder()
+                .bytes(bytes)
+                .type(contentType)
+                .build())
+            .flatMap(attachment -> storeAttachment(session, attachment)
+                .thenReturn(UploadResponse.builder()
+                    .blobId(attachment.getAttachmentId().getId())
+                    .type(attachment.getType())
+                    .size(attachment.getSize())
+                    .build()));
+    }
+
+    private Mono<byte[]> toBytesArray(InputStream inputStream) {
+        return Mono.fromCallable(() -> {
+            try {
+                return ByteStreams.toByteArray(inputStream);
+            } catch (IOException e) {
+                if (e instanceof EOFException) {
+                    throw new CancelledUploadException();
+                } else {
+                    throw new InternalErrorException("Error while uploading content", e);
+                }
+            }
+        });
+    }
+
+    private Mono<Object> storeAttachment(MailboxSession session, Attachment attachment) {
+        return Mono.fromRunnable(() -> {
+            try {
+                attachmentManager.storeAttachment(attachment, session);
+            } catch (MailboxException e) {
+                throw new InternalErrorException("Error while uploading content", e);
+            }
+        });
+    }
+
+    private Mono<Void> handleInternalError(HttpServerResponse response, Throwable e) {
+        LOGGER.error("Internal error", e);
+        return response.status(SC_INTERNAL_SERVER_ERROR).send();
+    }
+
+    private Mono<Void> handleCanceledUpload(HttpServerResponse response, CancelledUploadException e) {
+        LOGGER.info("An upload has been canceled before the end", e);
+        return response.send();
+    }
+
+    private Mono<Void> handleBadRequest(HttpServerResponse response, BadRequestException e) {
+        LOGGER.warn("Invalid authentication request received.", e);
+        return response.status(SC_BAD_REQUEST).send();
+    }
+}
