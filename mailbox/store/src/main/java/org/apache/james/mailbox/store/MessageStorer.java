5960b91bdca9e8b842b0922591e6b1b15f5ef02f
Benoit Tellier <btellier@linagora.com>
2020-04-19 16:52:12 +0700

    JAMES-2997 s/Attachment/AttachmentMetadata + s/MessageAttachment/MessageAttachmentMetadata

--- mailbox/store/src/main/java/org/apache/james/mailbox/store/MessageStorer.java
+++ mailbox/store/src/main/java/org/apache/james/mailbox/store/MessageStorer.java
@@ -30,7 +30,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import org.apache.james.mailbox.MailboxSession;
 import org.apache.james.mailbox.exception.MailboxException;
 import org.apache.james.mailbox.model.Mailbox;
-import org.apache.james.mailbox.model.MessageAttachment;
+import org.apache.james.mailbox.model.MessageAttachmentMetadata;
 import org.apache.james.mailbox.model.MessageId;
 import org.apache.james.mailbox.model.MessageMetaData;
 import org.apache.james.mailbox.model.ParsedAttachment;
@@ -48,7 +48,7 @@ public interface MessageStorer {
     /**
      * If applicable, this method will parse the messageContent to retrieve associated attachments and will store them.
      */
-    Pair<MessageMetaData, Optional<List<MessageAttachment>>> appendMessageToStore(Mailbox mailbox, Date internalDate, int size, int bodyStartOctet, SharedInputStream content, Flags flags, PropertyBuilder propertyBuilder, MailboxSession session) throws MailboxException;
+    Pair<MessageMetaData, Optional<List<MessageAttachmentMetadata>>> appendMessageToStore(Mailbox mailbox, Date internalDate, int size, int bodyStartOctet, SharedInputStream content, Flags flags, PropertyBuilder propertyBuilder, MailboxSession session) throws MailboxException;
 
     class WithAttachment implements MessageStorer {
         private static final Logger LOGGER = LoggerFactory.getLogger(WithAttachment.class);
@@ -72,19 +72,19 @@ public interface MessageStorer {
         }
 
         @Override
-        public Pair<MessageMetaData, Optional<List<MessageAttachment>>> appendMessageToStore(Mailbox mailbox, Date internalDate, int size, int bodyStartOctet, SharedInputStream content, Flags flags, PropertyBuilder propertyBuilder, MailboxSession session) throws MailboxException {
+        public Pair<MessageMetaData, Optional<List<MessageAttachmentMetadata>>> appendMessageToStore(Mailbox mailbox, Date internalDate, int size, int bodyStartOctet, SharedInputStream content, Flags flags, PropertyBuilder propertyBuilder, MailboxSession session) throws MailboxException {
             MessageMapper messageMapper = mapperFactory.getMessageMapper(session);
             MessageId messageId = messageIdFactory.generate();
 
             return mapperFactory.getMessageMapper(session).execute(() -> {
-                List<MessageAttachment> attachments = storeAttachments(messageId, content, session);
+                List<MessageAttachmentMetadata> attachments = storeAttachments(messageId, content, session);
                 MailboxMessage message = messageFactory.createMessage(messageId, mailbox, internalDate, size, bodyStartOctet, content, flags, propertyBuilder, attachments);
                 MessageMetaData metadata = messageMapper.add(mailbox, message);
                 return Pair.of(metadata, Optional.of(attachments));
             });
         }
 
-        private List<MessageAttachment> storeAttachments(MessageId messageId, SharedInputStream messageContent, MailboxSession session) throws MailboxException {
+        private List<MessageAttachmentMetadata> storeAttachments(MessageId messageId, SharedInputStream messageContent, MailboxSession session) throws MailboxException {
             List<ParsedAttachment> attachments = extractAttachments(messageContent);
             return attachmentMapperFactory.getAttachmentMapper(session)
                 .storeAttachmentsForMessage(attachments, messageId);
@@ -112,7 +112,7 @@ public interface MessageStorer {
         }
 
         @Override
-        public Pair<MessageMetaData, Optional<List<MessageAttachment>>> appendMessageToStore(Mailbox mailbox, Date internalDate, int size, int bodyStartOctet, SharedInputStream content, Flags flags, PropertyBuilder propertyBuilder, MailboxSession session) throws MailboxException {
+        public Pair<MessageMetaData, Optional<List<MessageAttachmentMetadata>>> appendMessageToStore(Mailbox mailbox, Date internalDate, int size, int bodyStartOctet, SharedInputStream content, Flags flags, PropertyBuilder propertyBuilder, MailboxSession session) throws MailboxException {
             MessageMapper messageMapper = mapperFactory.getMessageMapper(session);
             MessageId messageId = messageIdFactory.generate();
 
