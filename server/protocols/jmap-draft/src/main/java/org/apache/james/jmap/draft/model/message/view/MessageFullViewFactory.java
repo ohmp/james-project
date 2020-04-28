32bfd6dbf389d04e500b08ab224ede75b05df71c
Benoit Tellier <btellier@linagora.com>
2020-04-28 11:48:15 +0700

    JAMES-3149 Reactify MessageViewFactory

--- server/protocols/jmap-draft/src/main/java/org/apache/james/jmap/draft/model/message/view/MessageFullViewFactory.java
+++ server/protocols/jmap-draft/src/main/java/org/apache/james/jmap/draft/model/message/view/MessageFullViewFactory.java
@@ -92,18 +92,19 @@ public class MessageFullViewFactory implements MessageViewFactory<MessageFullVie
         return Helpers.toMessageViews(messages, this::fromMessageResults);
     }
 
-    public MessageFullView fromMetaDataWithContent(MetaDataWithContent message) throws IOException {
+    public Mono<MessageFullView> fromMetaDataWithContent(MetaDataWithContent message) throws IOException {
         Message mimeMessage = Helpers.parse(message.getContent());
         MessageContent messageContent = messageContentExtractor.extract(mimeMessage);
         Optional<String> htmlBody = messageContent.getHtmlBody();
         Optional<String> mainTextContent = messageContent.extractMainTextContent(htmlTextExtractor);
         Optional<String> textBody = computeTextBodyIfNeeded(messageContent, mainTextContent);
 
-        MessageFastViewPrecomputedProperties messageProjection = retrieveProjection(
-            messageContent,
-            message.getMessageId(),
-            () -> MessageFullView.hasAttachment(getAttachments(message.getAttachments())));
+        return retrieveProjection(messageContent, message.getMessageId(),
+                () -> MessageFullView.hasAttachment(getAttachments(message.getAttachments())))
+            .map(messageProjection -> instanciateMessageFullView(message, mimeMessage, htmlBody, textBody, messageProjection));
+    }
 
+    private MessageFullView instanciateMessageFullView(MetaDataWithContent message, Message mimeMessage, Optional<String> htmlBody, Optional<String> textBody, MessageFastViewPrecomputedProperties messageProjection) {
         return MessageFullView.builder()
                 .id(message.getMessageId())
                 .blobId(BlobId.of(blobManager.toBlobId(message.getMessageId())))
@@ -127,13 +128,11 @@ public class MessageFullViewFactory implements MessageViewFactory<MessageFullVie
                 .build();
     }
 
-    private MessageFastViewPrecomputedProperties retrieveProjection(MessageContent messageContent,
+    private Mono<MessageFastViewPrecomputedProperties> retrieveProjection(MessageContent messageContent,
                                                                     MessageId messageId, Supplier<Boolean> hasAttachments) {
         return Mono.from(fastViewProjection.retrieve(messageId))
             .onErrorResume(throwable -> fallBackToCompute(messageContent, hasAttachments, throwable))
-            .switchIfEmpty(computeThenStoreAsync(messageContent, messageId, hasAttachments))
-            .subscribeOn(Schedulers.elastic())
-            .block();
+            .switchIfEmpty(computeThenStoreAsync(messageContent, messageId, hasAttachments));
     }
 
     private Mono<MessageFastViewPrecomputedProperties> fallBackToCompute(MessageContent messageContent,
@@ -169,8 +168,12 @@ public class MessageFullViewFactory implements MessageViewFactory<MessageFullVie
             .orElse(message.getInternalDate());
     }
 
-    public MessageFullView fromMessageResults(Collection<MessageResult> messageResults) throws MailboxException, IOException {
-        return fromMetaDataWithContent(toMetaDataWithContent(messageResults));
+    Mono<MessageFullView> fromMessageResults(Collection<MessageResult> messageResults) {
+        try {
+            return fromMetaDataWithContent(toMetaDataWithContent(messageResults));
+        } catch (IOException | MailboxException e) {
+            return Mono.error(e);
+        }
     }
 
     private MetaDataWithContent toMetaDataWithContent(Collection<MessageResult> messageResults) throws MailboxException {
